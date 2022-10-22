fcfa41c894d37595c1b5c05821635c8684dec617
Martin <mgaspard.perso@gmail.com>
2022-10-22 18:52:34 +0200

    rendu

---
+++ script.js
@@ -0,0 +1,6 @@
+let video_minature = document.querySelector(".video_minature");
+let popup_container = document.querySelector(".popup_container");
+video_minature.addEventListener("click", function() {
+    popup_container.classList.toggle("active")
+})
+
